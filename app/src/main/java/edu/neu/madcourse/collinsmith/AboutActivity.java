37bbf1dba5fdeaf9178ea5efd05ab5ba35f9280b
Collin Smith <smith.colin@husky.neu.edu>
2019-05-22 13:24:33 -0700

    app code written

--- app/src/main/java/edu/neu/madcourse/collinsmith/AboutActivity.java
+++ app/src/main/java/edu/neu/madcourse/collinsmith/AboutActivity.java
@@ -1,14 +1,26 @@
 package edu.neu.madcourse.collinsmith;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPushed(v);
+            }
+        });
     }
 
     @Override
@@ -34,5 +46,15 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    public void buttonPushed(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        List explode = new ArrayList();
+        explodeHelper(explode);
+    }
+
+    public Object explodeHelper(List bomb){
+        return bomb.get(0);
     }
 }
